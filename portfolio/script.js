39f6f243b4a4ac6ccd9a309b2eabcac134688504
Chaitanya Rongali <rongalichaitanya@gmail.com>
2025-06-25 20:25:53 +0530

    Add files via upload

---
+++ portfolio/script.js
@@ -0,0 +1,12 @@
+
+document.getElementById('current-year').textContent = new Date().getFullYear();
+
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
